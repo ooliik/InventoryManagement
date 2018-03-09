499ea7311e4976af24a3f3dd760c15798d2fcab3
studemt <student@student.com>
2018-03-09 11:31:19 +0100

    initial

---
+++ Inventory management/Inventory management/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inventory_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
